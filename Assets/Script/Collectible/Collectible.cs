71cd46763bed8c075fa4d5b71ebc92c03ab08702
axeldeconti <axeldeconti@gmail.com>
2020-10-17 11:51:48 +0200

    Collectible début

---
+++ Assets/Script/Collectible/Collectible.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Collectible : MonoBehaviour
+{
+    [SerializeField] private GameObject _collectibleUI = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //Player is touched
+            GameObject col = Instantiate(_collectibleUI, transform.position, Quaternion.identity);
+            col.GetComponent<Image>().sprite = GetComponent<SpriteRenderer>().sprite;
+        }
+    }
+}
