3f665fee1a4d7c0d3efa6d773779069532c990c5
Tcartegnie <Sorakssis@gmail.com>
2020-10-17 22:24:46 +0200

    Add Gameover script

--- Assets/Script/PlateForme/GameOverPlateForme.cs
+++ Assets/Script/PlateForme/GameOverPlateForme.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class GameOverPlateForme : MonoBehaviour
 {
-
+	public Transform restart;
+	public Transform Player;
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		
+		Player.position = restart.position;	
 	}
 }
