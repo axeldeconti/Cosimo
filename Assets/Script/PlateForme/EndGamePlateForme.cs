696adf80fd544dd37b8b11d0a94fc50cc1d48eeb
Tcartegnie <Sorakssis@gmail.com>
2020-10-17 14:20:08 +0200

    Add an end game plateforme with camera transition

---
+++ Assets/Script/PlateForme/EndGamePlateForme.cs
@@ -0,0 +1,44 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EndGamePlateForme : MonoBehaviour
+{
+	public CinemachineVirtualCamera PlayerCamera;
+	public AnimationCurve Transition;
+	CinemachineTrackedDolly trackedDolly;
+	public float TransitionDuration;
+	public float CurrentCameraPosition;
+	public void Start()
+	{
+		trackedDolly = PlayerCamera.GetCinemachineComponent<CinemachineTrackedDolly>();
+	}
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		StartCoroutine(EndLevel());
+	}
+
+
+	IEnumerator EndLevel()
+	{
+		yield return StartCoroutine(EndLevelFeedBack());
+		yield return StartCoroutine(CameraTransition());
+	}
+	IEnumerator EndLevelFeedBack()
+	{
+		yield return new WaitForSeconds(1.0f);
+	}
+
+	IEnumerator CameraTransition()
+	{
+		float NextCameraPosition = CurrentCameraPosition + 1;
+		for(float i = CurrentCameraPosition; i < NextCameraPosition; i+= Time.deltaTime/ TransitionDuration)
+		{
+			trackedDolly.m_PathPosition = Transition.Evaluate(i);
+			yield return null;
+		}
+		CurrentCameraPosition = NextCameraPosition;
+	}
+}
