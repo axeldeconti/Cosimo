e1897e69f219ed7adcb7ec866c57ca1d7376b192
axeldeconti <axeldeconti@gmail.com>
2020-10-17 13:51:36 +0200

    Collectile done

--- Assets/Script/Collectible/Collectible.cs
+++ Assets/Script/Collectible/Collectible.cs
@@ -4,14 +4,17 @@ using UnityEngine.UI;
 public class Collectible : MonoBehaviour
 {
     [SerializeField] private GameObject _collectibleUI = null;
+    [SerializeField] private Transform _collectibleParent = null;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             //Player is touched
-            GameObject col = Instantiate(_collectibleUI, transform.position, Quaternion.identity);
+            Debug.Log("Collected");
+            GameObject col = Instantiate(_collectibleUI, Camera.main.WorldToScreenPoint(transform.position), Quaternion.identity, _collectibleParent);
             col.GetComponent<Image>().sprite = GetComponent<SpriteRenderer>().sprite;
+            Destroy(gameObject);
         }
     }
 }
