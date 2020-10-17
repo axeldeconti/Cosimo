55ac65aeeb393499b25e723ee03ca7df12daa0ec
axeldeconti <axeldeconti@gmail.com>
2020-10-17 20:09:17 +0200

    Clouds

---
+++ Assets/Script/Cloud.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    [SerializeField] private Vector2 _speedLimits = Vector2.zero;
+
+    private float _speed = 0;
+
+    public Vector3 destroyPos = Vector3.zero;
+
+    private void Start()
+    {
+        _speed = Random.Range(_speedLimits.x, _speedLimits.y);
+    }
+
+    private void Update()
+    {
+        transform.position += Vector3.right * _speed / 1000;
+
+        if (transform.position.x >= destroyPos.x)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Script/CloudSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    [SerializeField] private Vector2 _boxSize = Vector2.zero;
+    [SerializeField] private GameObject[] _cloudPrefabs = null;
+    [SerializeField] private Vector2 _spawnInterval = Vector2.zero;
+    [SerializeField] private float _destroyDistance = 0f;
+
+    private float _nextTimeToSpawn = 0;
+
+    private Vector3 _destroyPos => transform.position + Vector3.right * _destroyDistance;
+
+    private void Update()
+    {
+        if (_nextTimeToSpawn <= Time.time)
+        {
+            _nextTimeToSpawn = Time.time + Random.Range(_spawnInterval.x, _spawnInterval.y);
+            Vector3 pos = new Vector3(Random.Range(transform.position.x - _boxSize.x, transform.position.x + _boxSize.x), Random.Range(transform.position.y - _boxSize.y, transform.position.y + _boxSize.y), 1);
+            Instantiate(_cloudPrefabs[Random.Range(0, _cloudPrefabs.Length)], pos, Quaternion.identity, transform).GetComponent<Cloud>().destroyPos = _destroyPos;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position, _boxSize);
+        Gizmos.DrawWireSphere(_destroyPos, 1f);
+    }
+}
