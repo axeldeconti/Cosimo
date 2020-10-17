d2e22baa41edae28686ae97c057b8c2633161989
axeldeconti <axeldeconti@gmail.com>
2020-10-17 18:00:19 +0200

    Interactable, text ect

---
+++ Assets/Script/Interactable.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    [SerializeField] private GameObject _indicationParent = null;
+    [SerializeField] private bool _isActive = false;
+    [SerializeField] private bool _isDone = false;
+
+    public UnityEvent onInteract = null;
+
+    private void Start()
+    {
+        _isActive = false;
+        _isDone = false;
+        _indicationParent.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!_isActive)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            onInteract.Invoke();
+            _isDone = true;
+            _isActive = false;
+            _indicationParent.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !_isDone)
+        {
+            _isActive = true;
+            _indicationParent.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !_isDone)
+        {
+            _isActive = false;
+            _indicationParent.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Script/TextManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class TextManager : MonoBehaviour
+{
+    private AudioSource _aSource = null;
+
+    [SerializeField] private GameObject _textParent = null;
+    [SerializeField] private TextMeshProUGUI _text = null;
+    [SerializeField] private Sentence _intro;
+    [SerializeField] private Sentence[] _sentences = null;
+
+    private void Start()
+    {
+        _aSource = GetComponent<AudioSource>();
+
+        _textParent.SetActive(false);
+    }
+
+    public void PlayIntro()
+    {
+        _text.text = _intro.text;
+        _aSource.clip = _intro.audio;
+        _aSource.Play();
+    }
+
+    public void PlaySentence(int level)
+    {
+        _textParent.SetActive(true);
+        _text.text = _sentences[level].text;
+        _aSource.clip = _sentences[level].audio;
+        _aSource.Play();
+    }
+
+    public void Hide()
+    {
+        _textParent.SetActive(false);
+    }
+}
+
+[Serializable]
+public struct Sentence
+{
+    [TextArea(2, 4)]
+    public string text;
+    public AudioClip audio;
+}
