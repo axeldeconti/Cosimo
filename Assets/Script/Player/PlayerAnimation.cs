dd364010303ed654c0c6d666d749b032f37ca1b3
axeldeconti <axeldeconti@gmail.com>
2020-10-17 14:47:16 +0200

    Sprites

---
+++ Assets/Script/Player/PlayerAnimation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private PlayerMove _move = null;
+    private Animator _anim = null;
+
+    private bool _isGrounded = false;
+
+    private void Start()
+    {
+        _move = GetComponent<PlayerMove>();
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _anim.SetFloat("XVelocity", 0);
+        _anim.SetFloat("YVelocity", 0);
+
+        if(_move.OnGround && !_isGrounded)
+            _anim.SetTrigger("IsGrouned");
+    }
+}
