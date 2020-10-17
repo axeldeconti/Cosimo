18e438a98d2a116d1323ae6eeb99c14eeb9873ed
Tcartegnie <Sorakssis@gmail.com>
2020-10-17 22:27:30 +0200

    Refacto player animation

--- Assets/Script/Player/PlayerAnimation.cs
+++ Assets/Script/Player/PlayerAnimation.cs
@@ -2,23 +2,26 @@
 
 public class PlayerAnimation : MonoBehaviour
 {
-    private PlayerMove _move = null;
-    private Animator _anim = null;
+    public PlayerMove _move;
+    public Animator _anim;
 
     private bool _isGrounded = false;
 
     private void Start()
     {
-        _move = GetComponent<PlayerMove>();
-        _anim = GetComponent<Animator>();
+      //  _move = GetComponent<PlayerMove>();
+     //   _anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        _anim.SetFloat("XVelocity", 0);
-        _anim.SetFloat("YVelocity", 0);
+        _anim.SetFloat("XVelocity", _move.GetXVelocity());
+        _anim.SetFloat("YVelocity", _move.GetYVelocity());
+
 
         if(_move.OnGround && !_isGrounded)
             _anim.SetTrigger("IsGrouned");
+
+        _isGrounded = _move.OnGround;
     }
 }
