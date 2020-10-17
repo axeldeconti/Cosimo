02c1c0be8841a7e7526eb0b080f4177eec9bcd15
Tcartegnie <Sorakssis@gmail.com>
2020-10-17 11:02:49 +0200

    Add a sticky plateforme

---
+++ Assets/Script/PlateForme/StickyPlateForme.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlateForme : MonoBehaviour
+{
+	public Rigidbody2D CurrentRB;
+	[Range(0.0f, 1.0f)]
+	public float StickyIntensity;
+
+	private void Update()
+	{
+		if(CurrentRB != null)
+		{
+			float CurrentXVelocity = CurrentRB.velocity.x;
+			CurrentRB.velocity -= new Vector2(CurrentXVelocity * StickyIntensity,0);
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		CurrentRB = collision.gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+		CurrentRB = null;
+	}
+}
