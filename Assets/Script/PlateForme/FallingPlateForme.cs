00eb67e230754e15a6e7b69f775ffc3c11c67ea4
Tcartegnie <Sorakssis@gmail.com>
2020-10-17 11:21:32 +0200

    Add script of falling plateforme

---
+++ Assets/Script/PlateForme/FallingPlateForme.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlateForme : MonoBehaviour
+{
+	public Rigidbody2D RB;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		RB.constraints = RigidbodyConstraints2D.None;
+		RB.constraints = RigidbodyConstraints2D.FreezePositionX;
+	}
+}
