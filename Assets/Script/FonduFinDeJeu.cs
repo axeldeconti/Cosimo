920005a826b48b8cc31869844ea4d60510890e09
Tcartegnie <Sorakssis@gmail.com>
2020-10-18 14:16:57 +0200

    Add a fade

---
+++ Assets/Script/FonduFinDeJeu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FonduFinDeJeu : MonoBehaviour
+{
+    public Image Picture;
+    public AnimationCurve curve;
+    // Start is called before the first frame update
+    public void StartFade()
+	{
+        StartCoroutine(FadeIn());
+	}
+
+  IEnumerator FadeIn()
+	{
+        for(float i = 1; i >0; i-= Time.deltaTime)
+		{
+            Picture.color = new Color(Picture.color.r, Picture.color.g, Picture.color.b, curve.Evaluate(i));
+            yield return null;
+        }
+        Picture.color = new Color(Picture.color.r, Picture.color.g, Picture.color.b, curve.Evaluate(1));
+    }
+}
