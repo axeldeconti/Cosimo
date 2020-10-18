05e414970e4845ac54b7a88a1d0de778fbfe3738
axeldeconti <axeldeconti@gmail.com>
2020-10-18 11:09:43 +0200

    Dog and bird

---
+++ Assets/Script/DogAndBird.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogAndBird : MonoBehaviour
+{
+    private Animator _anim = null;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public void Trigger()
+    {
+        _anim.SetTrigger("trigger");
+    }
+}
--- Assets/Script/PlateForme/BouncingPlateForme.cs
+++ Assets/Script/PlateForme/BouncingPlateForme.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BouncingPlateForme : MonoBehaviour
 {
 	public float BouncingForce;
+	public UnityEvent onBounce = null;
 
 	public void OnCollision(Rigidbody2D CollidingObject,Vector3 BouncingNormalSurface)
 	{
 		CollidingObject.AddForce(-BouncingNormalSurface * BouncingForce);
+		onBounce.Invoke();
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision)
