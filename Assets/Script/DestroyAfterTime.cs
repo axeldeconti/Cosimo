f54a4c014f54de5da6fc95d390ab72f316d7a04d
axeldeconti <axeldeconti@gmail.com>
2020-10-18 02:04:57 +0200

    1 Level ok

---
+++ Assets/Script/DestroyAfterTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour
+{
+    [SerializeField] private float _time = 0;
+
+    private float _timeToBoom = 0;
+
+    private void Start()
+    {
+        _timeToBoom = Time.time + _time;
+    }
+
+    private void Update()
+    {
+        if (Time.time > _timeToBoom)
+            Destroy(gameObject);
+    }
+}
--- Assets/Script/PlateForme/PlateformeParent.cs
+++ Assets/Script/PlateForme/PlateformeParent.cs
@@ -2,5 +2,13 @@
 
 public class PlateformeParent : MonoBehaviour
 {
+    [SerializeField] private GameObject _particle = null;
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Instantiate(_particle, transform.position, Quaternion.identity, transform);
+        }
+    }
 }
