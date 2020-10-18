e7970d84ca885f5fa258c3dc3d647a9b3d9b3793
axeldeconti <axeldeconti@gmail.com>
2020-10-18 09:46:39 +0200

    Hey tag grounded

---
+++ Assets/Script/ActionOnTriggerEnter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionOnTriggerEnter : MonoBehaviour
+{
+    public UnityEvent onTriggerEnter = null;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            onTriggerEnter.Invoke();
+    }
+}
