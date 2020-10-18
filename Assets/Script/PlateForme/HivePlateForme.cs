89cc3f432f61652ab82faeb58ad2b82a97cb7a85
Tcartegnie <Sorakssis@gmail.com>
2020-10-18 11:58:07 +0200

    Add a "Bounc count" plateforme

---
+++ Assets/Script/PlateForme/HivePlateForme.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HivePlateForme : MonoBehaviour
+{
+	int BouncCount;
+	public int ExcpectedBounceCount;
+	bool BounceEnable;
+
+
+
+	public UnityEvent events;
+	public UnityEvent eventsOnBounce;
+
+	public void BounceOn()
+	{
+		BounceEnable = true;
+	}
+
+	void BounceOff()
+	{
+		BounceEnable = false;
+	}
+
+	public void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(BounceEnable)
+		{
+			BouncCount++;
+			BounceOff();
+			eventsOnBounce.Invoke();
+		}
+	}
+
+
+	public void Update()
+	{
+		if(BouncCount >= ExcpectedBounceCount)
+		{
+			events.Invoke();
+			BouncCount = 0;
+		}
+	}
+
+}
